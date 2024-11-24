a3b602c94e3738bf701f44a514338ef092801299
Gregory Bouteiller <gregory.bouteiller@gmail.com>
2024-11-24 14:58:43 +0400

    up

--- src/components/records-items.tsx
+++ src/components/records-items.tsx
@@ -1,7 +1,10 @@
 import type { Item } from "@/lib/pocketbase/utils";
+import { cn } from "@/lib/utils";
+import { TITLE } from "@/styles/ui";
 import { Image } from "@unpic/react";
-import { RecordsCarousel } from "./records-carousel";
+import { useEffect, useMemo, useState } from "react";
 import { BUTTON } from "./ui/button";
+import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious, type CarouselApi } from "./ui/carousel";
 import { Features } from "./ui/features";
 import { Section, type SectionProps } from "./ui/section";
 import { Title } from "./ui/title";
@@ -34,7 +37,22 @@ export function RecordsItems(props: RecordsItemsProps) {
     "calc(100vw - 7rem - 15px)",
   ].join(", ");
 
-  const target = externalLink ? "_blank" : "_self";
+  const target = useMemo(() => (externalLink ? "_blank" : undefined), [externalLink]);
+  const filteredItems = useMemo(
+    () => (removeStale ? items.filter(({ stale }) => stale && stale >= new Date().toISOString()) : items),
+    [items, removeStale],
+  );
+
+  const [api, setApi] = useState<CarouselApi>();
+  const [justifyCenter, setJustifyCenter] = useState(false);
+  const [isSet, setIsSet] = useState(false);
+
+  useEffect(() => {
+    if (!api) return;
+    setJustifyCenter(api.scrollSnapList().length === 1);
+    setIsSet(true);
+    api.on("reInit", ({ scrollSnapList }) => setJustifyCenter(scrollSnapList().length === 1));
+  }, [api]);
 
   return (
     <>
@@ -57,9 +75,32 @@ export function RecordsItems(props: RecordsItemsProps) {
       ) : (
         <Section intent={intent} expanded={hasSome} {...rest} header={<Title text={title} className="mb-8" />}>
           {hasSome ? (
-            <RecordsCarousel externalLink={externalLink} items={items} removeStale={removeStale} className="w-full">
-              {children}
-            </RecordsCarousel>
+            <Carousel setApi={setApi} opts={{ loop: true }} className="w-full">
+              <CarouselContent
+                className={cn("opacity-0 transition-opacity duration-700", { "justify-center": justifyCenter, "opacity-100": isSet })}
+              >
+                {filteredItems.map(({ features, href, image, slug, text, title }) => (
+                  <CarouselItem key={slug} className="mb-2 max-w-96">
+                    <div className="flex h-full w-full flex-col bg-white shadow-md">
+                      <Image {...image} height={256} width={384} breakpoints={[384, 768]} sizes="24rem" className="flex-none" />
+                      <div className="flex flex-1 flex-col gap-4 p-4 px-6 sm:px-8">
+                        <h4 className={TITLE()}>{title}</h4>
+                        <Features features={features} intent="white" />
+                        <article dangerouslySetInnerHTML={{ __html: text }} className="prose prose-p:my-1 prose-p:leading-normal" />
+                        <div className="flex-1" />
+                        <div className="flex justify-end gap-2">
+                          <a href={href} target={target} className={BUTTON()}>
+                            En savoir plus
+                          </a>
+                        </div>
+                      </div>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious className="left-4" />
+              <CarouselNext className="right-4" />
+            </Carousel>
           ) : (
             children
           )}
